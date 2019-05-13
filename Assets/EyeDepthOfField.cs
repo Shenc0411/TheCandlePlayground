8697fc520d68c9ab75398db6180f020db9802901
Cong Shen <shenc0411@gmail.com>
2019-05-12 23:32:46 -0700

    Experimenting with eye tracking DOF and selection

---
+++ Assets/EyeDepthOfField.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Tobii.Gaming;
+using UnityEngine.Rendering.PostProcessing;
+
+public class EyeDepthOfField : MonoBehaviour
+{
+
+    private PostProcessVolume _postProcessVolume;
+    private DepthOfField DOF;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _postProcessVolume = GetComponent<PostProcessVolume>();
+        _postProcessVolume.profile.TryGetSettings(out DOF);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GazePoint gazePoint = TobiiAPI.GetGazePoint();
+        Ray ray = Camera.main.ScreenPointToRay(gazePoint.Screen);
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            DOF.focusDistance.value = Mathf.Lerp(hit.distance, DOF.focusDistance.value, Time.deltaTime);
+            Debug.Log(DOF.focusDistance.value);
+        }
+    }
+}
---
+++ Assets/GazeSelection.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Tobii.Gaming;
+
+public class GazeSelection : MonoBehaviour
+{
+
+    public Material matNotSelected;
+    public Material matSelected;
+
+    private GazeAware _gazeAware;
+    private MeshRenderer _meshRenderer;
+    private bool isBeingDragged = false;
+    private float distance = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _gazeAware = GetComponent<GazeAware>();
+        _meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (_gazeAware.HasGazeFocus)
+        {
+            _meshRenderer.material = matSelected;
+            if (Input.GetKey(KeyCode.Space))
+            {
+                isBeingDragged = true;
+                distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+            }
+            else
+            {
+                isBeingDragged = false;
+            }
+
+            if (isBeingDragged)
+            {
+                GazePoint gazePoint = TobiiAPI.GetGazePoint();
+                Ray ray = Camera.main.ScreenPointToRay(gazePoint.Screen);
+                transform.position = Camera.main.transform.position +  distance * ray.direction.normalized;
+                transform.LookAt(Camera.main.transform.position);
+            }
+
+        }
+        else
+        {
+            _meshRenderer.material = matNotSelected;
+        }
+    }
+}
